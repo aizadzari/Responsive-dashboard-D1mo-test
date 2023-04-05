2a1e653e453d7a0f8206dd2b5787a7ff4bfa4252
aizadzari <aizadzari@gmail.com>
2023-04-06 05:18:13 +0800

    change logo

--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -1,12 +1,14 @@
 import React from 'react'
 // import { Link } from 'react-router-dom'
+import img from "../../img/logo.png"
 
 const Navbar = () => {
     return (
         <React.Fragment>
             <div className='d-flex justify-content-between align-items-center px-5 py-3'>
-                <h4>LOGO</h4>
-
+                <div className='logo-container'>
+                <img alt='logo' src={img} />
+                </div>
                 {/* <div className='d-flex justify-content-between align-items-center'>
                     <div className='me-4'><Link>Home</Link></div>
                     <div className='me-4'><Link>About</Link></div>
