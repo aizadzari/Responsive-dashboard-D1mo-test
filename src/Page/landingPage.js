9bd9998b621dddfa410c8452b267f9de7f9b9d02
aizadzari <aizadzari@gmail.com>
2023-05-05 15:19:23 +0800

    fix layout

--- src/Page/landingPage.js
+++ src/Page/landingPage.js
@@ -5,7 +5,7 @@ import Navbar from '../Components/Layout/Navbar'
 export const landingPage = () => {
   return (
     <React.Fragment>
-      <div className='container px-lg-5 py-lg-3' >
+      <div className='container px-lg-5 py-lg-3 overflow-hidden' >
         <Navbar />
         <Content />
       </div>
