56c18937eb3a73d96a7cda5fcff7080e8a4a2abb
aizadzari <aizadzari@gmail.com>
2026-09-15 17:59:50 +0800

    update Header

--- src/Components/Layout/Content.js
+++ src/Components/Layout/Content.js
@@ -96,8 +96,12 @@ const Content = () => {
                                         <h1 className='animate-box-1 fw-bold' ref={setBoxRef(0)} style={{ transitionDelay: "300ms" }}>Hey, I'm Aizad,</h1>
                                         <h1 className='animate-box-2 fw-bold' ref={setBoxRef(1)} style={{ transitionDelay: "400ms" }}>a <span className='text-secondary'>Frontend Developer</span> with <span className='text-secondary'>7+ years</span> <br /> of experience</h1>
 
-                                        <p className='small mt-4 animate-box-3' ref={setBoxRef(2)} style={{ transitionDelay: "500ms" }}>As an experienced software developer, I possess a diverse and promising skillset that enables me to bring forth expertise in designing, developing, and maintaining software systems.</p>
-
+                                        {/* <p className='small mt-4 animate-box-3' ref={setBoxRef(2)} style={{ transitionDelay: "500ms" }}>As an experienced frontend developer, I possess a diverse and promising skillset that enables me to bring forth expertise in designing, developing, and maintaining software systems.</p> */}
+<p className='small mt-4 animate-box-3' ref={setBoxRef(2)} style={{ transitionDelay: "500ms" }}>Experience designing and building scalable web applications, digital
+platforms, and user-centered interfaces. Proven expertise in frontend and backend development, UI/UX design,
+and digital transformation projects. Skilled in translating complex requirements into intuitive, accessible, and
+visually compelling applications for diverse industries including healthcare, logistics, oil & gas, and government
+sectors.</p>
                                         <div className='mt-5 bottom-1 left-1'>
                                             <button className='btn btn-light rounded-pill me-2' onClick={handleLinkedIn}><i className="ri-linkedin-fill align-middle"></i></button>
                                             <button className='btn btn-light rounded-pill' onClick={handleGithub}><i className="ri-github-fill"></i></button>
